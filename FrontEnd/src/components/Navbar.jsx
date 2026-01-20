ce08386898292ff3527b5247a797158b999ea281
Husain Ansari <ansarihusain9510@gmail.com>
2026-01-20 04:39:09 -0800

    Rename TaskFlow to ProjectFlow (branding update)

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         
         <h3 className="text-xl font-bold text-indigo-600">
-          TaskFlow
+          ProjectFlow
         </h3>
 
         
